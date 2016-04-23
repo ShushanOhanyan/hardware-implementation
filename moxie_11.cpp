a623172bc6926c62308cf74d6ff3546442a655b7
Kar111 <avetisyankarine111@gmail.com>
2016-04-23 22:24:17 +0400

    Added files via upload

---
+++ moxie_11.cpp
@@ -0,0 +1,40 @@
+void and(int& rA,int rB)
+{
+   rA=rA&rB;
+}
+void or(int& rA,int rB)
+{
+   rA=rA|rB;
+}
+void xor(int& rA,int rB)
+{
+   rA=rA^rB;
+}
+void ashl(int& rA,int rB)
+{
+rA=rA<<rB;
+}
+
+void ashr(int& rA,int rB)
+{
+rA=rA>>rB;
+}
+
+void lshr(int& rA,int rB)
+{
+  int mask=((1<<31)>>rB)<<1;
+  rA=(rA>>rB)&(~mask);
+}
+void sex_b(int& rA,int rB)
+{
+ rA=(rB<<24)>>24;
+}
+void sex_s(int& rA,int rB)
+{
+rA=(rB<<16)>>16;
+}
+
+void not(int& rA,int rB)
+{
+  rA=!rB;
+}
