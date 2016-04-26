f824eb28d41d68e28997eb10509b223f1175cde4
Spartak Shahbazyan <shahbazyan.spartak@gmail.com>
2016-04-26 11:46:21 +0400

    Added files via upload

---
+++ Spartak.cpp
@@ -0,0 +1,65 @@
+void stb(string ra, string rb)
+{
+	int rara = genReg[0];
+	int rbrb = genReg[1];
+	
+	*((int*)rara)=(char)rbrb;
+}
+
+void stl(string ra,string rb)
+{
+	int rara = genReg[0];
+	int rbrb = genReg[1];
+	*((int*)rara)=rbrb;
+}
+
+void sts(string ra, string rb)
+{
+	int rara = genReg[0];
+	int rbrb = genReg[1];
+	*((int*)ra)=(short)rbrb;
+}
+
+void stab(string ra, int rb)
+{
+	int rara = genReg[0];
+	*((int*)rb) = (char)rara;
+}
+
+void stal(string ra, int rb)
+{
+	int rara = genReg[0];
+	*((int*)rb) = rara;
+}
+
+void stas(string ra, int rb)
+{
+	int rara = genReg[0];
+	*((int*)rb) = short(rara)
+}
+
+void stob(string ra,)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
