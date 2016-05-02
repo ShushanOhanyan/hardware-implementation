2cd25295d96186b3f705a19c408653486d66feab
Spartak Shahbazyan <shahbazyan.spartak@gmail.com>
2016-05-02 11:32:16 +0400

    Update Spartak.cpp

--- Spartak.cpp
+++ Spartak.cpp
@@ -1,40 +1,40 @@
 void stb(string ra, string rb)
 {
-	int rara = genReg[0];
-	int rbrb = genReg[1];
+	int rara = genReg[ra];
+	int rbrb = genReg[rb];
 	
 	*((int*)rara)=(char)rbrb;
 }
 
 void stl(string ra,string rb)
 {
-	int rara = genReg[0];
-	int rbrb = genReg[1];
+	int rara = genReg[ra];
+	int rbrb = genReg[rb];
 	*((int*)rara)=rbrb;
 }
 
 void sts(string ra, string rb)
 {
-	int rara = genReg[0];
-	int rbrb = genReg[1];
+	int rara = genReg[ra];
+	int rbrb = genReg[rb];
 	*((int*)ra)=(short)rbrb;
 }
 
 void stab(string ra, int rb)
 {
-	int rara = genReg[0];
+	int rara = genReg[ra];
 	*((int*)rb) = (char)rara;
 }
 
 void stal(string ra, int rb)
 {
-	int rara = genReg[0];
+	int rara = genReg[ra];
 	*((int*)rb) = rara;
 }
 
 void stas(string ra, int rb)
 {
-	int rara = genReg[0];
+	int rara = genReg[ra];
 	*((int*)rb) = short(rara)
 }
 
